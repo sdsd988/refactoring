6c4f78df30ddc14383dd2fa3f69ad19e0eb408d1
JUNGSANGYUN <sdsd98987@gmail.com>
2025-01-18 10:51:22 +0900

    1.6 계산 단계와 포맷팅 단계 분리하기
    - renderPlainText() 안에서 totalAmount(), totalVolumeCredits()에서 사용하는 방식 대신 매개변수 전달 방식으로 전환
    
    - totalAmount() 함수도 StatementData 클래스로 이동

--- src/main/java/toby/salon/refactoring/chapter1/dto/StatementData.java
+++ src/main/java/toby/salon/refactoring/chapter1/dto/StatementData.java
@@ -1,15 +1,10 @@
 package toby.salon.refactoring.chapter1.dto;
 
-import java.util.List;
 
 public record StatementData(
         Invoice invoice,
         Plays plays
 ) {
-
-    public List<Performance> getPerformances() {
-        return invoice.performances();
-    }
 
     public Play playFor(Performance performance) {
         return plays.get(performance.playId());
@@ -52,5 +47,13 @@ public record StatementData(
             volumeCredits += volumeCreditsFor(perf);
         }
         return volumeCredits;
+    }
+
+    public int totalAmount() throws Exception {
+        int result = 0;
+        for (Performance perf : invoice().performances()) {
+            result += amountFor(perf);
+        }
+        return result;
     }
 }
