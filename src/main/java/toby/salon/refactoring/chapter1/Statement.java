a24c62209913098e332d4b4358868bcdaebc8fbe
JUNGSANGYUN <sdsd98987@gmail.com>
2025-01-18 11:18:02 +0900

    1.6 계산 단계와 포맷팅 단계 분리하기
    - html 버전 작성 준비

--- src/main/java/toby/salon/refactoring/chapter1/Statement.java
+++ src/main/java/toby/salon/refactoring/chapter1/Statement.java
@@ -17,6 +17,21 @@ public class Statement {
         return renderPlainText(data);
     }
 
+    private String htmlStatement(Invoice invoice, Plays plays) throws Exception {
+        return renderHtml(data);
+    }
+
+    private String renderHtml(StatementData data) throws Exception {
+        StringBuilder result = new StringBuilder(String.format("청구내역 (고객명: %s)\n", data.invoice().customer()));
+        for (Performance performance : data.invoice().performances()) {
+            result.append(String.format("%s: $%d %d석\n",data.playFor(performance).name(), data.amountFor(performance) / 100, performance.audience()));
+        }
+
+        result.append(String.format("총액: $%d\n", data.totalAmount()));
+        result.append(String.format("적립 포인트: %d점", data.totalVolumeCredits()));
+        return result.toString();
+    }
+
     private String renderPlainText(StatementData data) throws Exception {
         StringBuilder result = new StringBuilder(String.format("청구내역 (고객명: %s)\n", data.invoice().customer()));
 
